d350e97bda8728be87264227d820f4a51aa1588c
Eduardo Rivas <eduardrivs@gmail.com>
2022-07-13 23:00:54 -0500

    Arregle los picker para que funcionen con la info de la BD y las rutas htttps seguras

---
+++ AppEvaluacion3WEB1.3/AppEvaluacion3WEB1.3/Entities/CatEstadoVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppEvaluacion3WEB1._3.Entities
+{
+    public class CatEstadoVM
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
